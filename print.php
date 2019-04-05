1d25923be3dea1470640db76a9d712bbfcb3cbb8
RishikaVarshiiniRao <43945485+RishikaVarshiiniRao@users.noreply.github.com>
2019-04-05 10:49:39 +0530

    Add files via upload

---
+++ print.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Print</title>
+</head>
+
+<body>
+
+<?php $txt ="Minions";
+echo "I don't like $txt";
+?>
+
+</body>
+
+</html>
---
+++ variables.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Variables</title>
+</head>
+
+<body>
+
+<?php $txt ="Hello World";
+$x=5;
+$y=10.5;
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+
+?>
+
+</body>
+
+</html>
